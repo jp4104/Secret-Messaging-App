e35218c6c31ba59f407b8c7303c60051c7bf0476
Paul G <paulgellatly@icloud.com>
2020-03-23 10:58:55 +0000

    revised

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ document.querySelector('form').addEventListener('submit', (event) => {
 	document.querySelector('#link-form').classList.remove('hide');
 
 	const input = document.querySelector('#message-input');
-
 	const encrypted = btoa(input.value);
 
 	const linkInput = document.querySelector('#link-input');
-
 	linkInput.value = `${window.location}#${encrypted}`;
 	linkInput.select();
 });
